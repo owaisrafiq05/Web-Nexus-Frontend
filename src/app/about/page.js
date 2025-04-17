f7ce5284530ef1193e6b3b17d1cc019e02047791
owaisrafiq05 <owaisrafiq05@gmail.com>
2025-04-17 12:01:30 +0500

    Why Us Feature Comp Added

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import AboutHero from "@/components/AboutComponents/AboutHero";
+import WhyUsFeatures from "@/components/AboutComponents/WhyUsFeature";
+import Services from "@/components/HomeComponents/Services";
+import WhoWeAre from "@/components/HomeComponents/WhoWeAre";
 
 export default function About() {
   return (
     <div>
       <AboutHero/>
+      <Services/>
+      <WhoWeAre/>
+      <WhyUsFeatures/>
     </div>
   );
 }
