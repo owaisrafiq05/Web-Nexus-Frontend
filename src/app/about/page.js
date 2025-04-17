4ede2a1f85f8809b63b9650219f1254f039b3e5e
owaisrafiq05 <owaisrafiq05@gmail.com>
2025-04-17 11:54:13 +0500

    About Hero Added

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import AboutHero from "@/components/AboutComponents/AboutHero";
+
+export default function About() {
+  return (
+    <div>
+      <AboutHero/>
+    </div>
+  );
+}
